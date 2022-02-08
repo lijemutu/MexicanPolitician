5c186d7748c703850598c72cc5f00a2188041b3a
lijemutu <erlowarlo@gmail.com>
2022-02-08 14:57:21 -0600

    unit tests added and working controller for getting info

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
-from flask import Flask
+from flask import Flask, request
+
+from APIs.names_api import NamesApi
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/api/name",methods = ["GET"])
+def NameNationality():
+    args = request.args
+
+    if "fn" in args:
+        firstName = args["fn"]
+
+    if "sn" in args:
+        lastName = args.get("sn")
+
+    if "ssn" in args:
+        secondLastName = args["ssn"]
+    if firstName == "" or lastName == "":
+        return "You miss firstName or lastName",400
+    nameObject = NamesApi(firstName,lastName,secondLastName=secondLastName)
+    nationalities = nameObject.request()
+    
+    return nationalities,200
---
+++ tests/api_test.py
@@ -0,0 +1,19 @@
+from ast import Assert
+import unittest
+
+from itsdangerous import json
+
+from APIs.names_api import NamesApi
+
+class TestSuite(unittest.TestCase):
+
+    def test_NamesApi(self):
+        # Código que se quiere probar
+        name = "Andres"
+        lastName = "López"
+        secondLastName = "Obrador"
+
+        nationality = NamesApi(name,lastName,secondLastName=secondLastName)
+        jsonResponse = nationality.request()
+
+        self.assertNotEqual(jsonResponse,{})
