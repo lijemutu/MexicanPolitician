400d39d14eb7c94f48e7ff64faca86e40c2e1ad3
lijemutu <erlowarlo@gmail.com>
2022-02-08 12:44:02 -0600

    added secret for names api and classes for external apis

---
+++ APIs/Api.py
@@ -0,0 +1,3 @@
+class Api:
+    def request(self):pass
+    def save(self):pass
--- APIs/names_api.py
+++ APIs/names_api.py
@@ -1,15 +1,25 @@
 import requests
+from APIs.Api import Api
+from dotenv import load_dotenv
+import os 
 
-class NamesApi:
+load_dotenv()
+API_KEY_NAMES = os.environ.get('API_KEY_NAMES')
 
+class NamesApi(Api):
 
-    def __init__(self,firstName,lastName) -> None:
+    def __init__(self,firstName,lastName,secondLastName = "") -> None:
         self.firstName = firstName
         self.lastName = lastName
+        self.secondLastName = secondLastName
+        
 
-    def requestNationality(self):
-        ONO_API = "https://ono.4b.rs/v1/nat"
+
+    def request(self):
+        ONO_API = f"https://ono.4b.rs/v1/nat?key={API_KEY_NAMES}&fn={self.firstName}&sn={self.lastName}&ssn={self.secondLastName}"
         r = requests.get(ONO_API)
         if(r.status_code != 200):
             raise Exception(f"Request for name ... not valid")
+        nationalityData = r.json()
+        return nationalityData
 
